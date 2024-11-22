4fcfb2191af676caadb604b3c1334097bf419415
kpa991 <pklimov41@gmail.com>
2024-11-22 14:18:54 +0300

    выполнил задание 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            (string Name,string[] LoveEat ) User;
+
+            Console.Write("Введите ваше имя:");
+            User.Name = Console.ReadLine();
+
+            User.LoveEat = new string[5];
+            for (int i = 0; i<User.LoveEat.Length; i++)
+            {
+                int n = i + 1;
+                Console.WriteLine("Напишите ваше " + n + " любимое блюдо?");
+                User.LoveEat[i] = Console.ReadLine();
+            }
+
+
         }
     }
 }
