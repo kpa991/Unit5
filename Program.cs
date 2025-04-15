19db42e6a25eba10c9cb9d5d2f6e973b6d9666a7
kpa991 <pklimov41@gmail.com>
2025-04-15 15:47:36 +0300

    Выполнил задание 5.1.5

--- Program.cs
+++ Program.cs
@@ -2,22 +2,70 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        static string ShowColor()
         {
-            (string Name,string[] LoveEat ) User;
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color = Console.ReadLine();
 
-            Console.Write("Введите ваше имя:");
-            User.Name = Console.ReadLine();
-
-            User.LoveEat = new string[5];
-            for (int i = 0; i<User.LoveEat.Length; i++)
+            switch (color)
             {
-                int n = i + 1;
-                Console.WriteLine("Напишите ваше " + n + " любимое блюдо?");
-                User.LoveEat[i] = Console.ReadLine();
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+            return color;
+        }
+        public static void Main(string[] args)
+        {
+
+            var (name, age) = ("Евгения", 27);
+
+            Console.WriteLine("Мое имя: {0}", name);
+            Console.WriteLine("Мой возраст: {0}", age);
+
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите возрас с цифрами:");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", name);
+            Console.WriteLine("Ваш возраст: {0}", age);
+
+            var favcolors = new string[3];
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor();
             }
 
-
+            Console.WriteLine("Ваши любимые цвета:");
+            foreach (var color  in favcolors)
+            {
+                Console.WriteLine(color);
+            }
+            Console.ReadKey();
         }
     }
 }
