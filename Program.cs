1440c35619b4ac3cf134f74d77a59f808e5fe8ba
kpa991 <pklimov41@gmail.com>
2025-04-15 16:05:15 +0300

    Выполнил задание 5.1.6

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@
 
             GetArrayFromConsole();
 
-            Console.ReadKey();
         }
         static int[] GetArrayFromConsole()
         {
@@ -22,8 +21,24 @@
             }
 
             int temp = 0;
+            for (int i =0; i<result.Length; i++)
+            {
+                for (int j=i+1; j<result.Length; j++)
+                {
+                    if (result[i]> result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+                }
+            }
 
-
+            Console.WriteLine("Вывод отсортированного массива:");
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
             return result;
         }
     }
